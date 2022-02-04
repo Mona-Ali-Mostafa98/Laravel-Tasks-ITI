6b6f14dc69fbdf16657e32028e42baec0d921964
Mona Ali Mostafa <monaali980628@gmail.com>
2022-02-04 23:13:27 +0200

    Add Laravel Sanctum

--- All Tasks of Laravel/routes/api.php
+++ All Tasks of Laravel/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,7 +22,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('posts',[PostController::class, 'index'])->name('api.posts.index');
+Route::get('posts',[PostController::class, 'index'])->name('api.posts.index')-> middleware('auth:sanctum');
 Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::post('posts',[PostController::class, 'store'])->name('posts.store');
 
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
