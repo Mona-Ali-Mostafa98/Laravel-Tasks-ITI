cd25971cff0110941b40f2ac84042c1c471bd42e
Mona Ali Mostafa <monaali980628@gmail.com>
2022-02-04 22:16:21 +0200

    create PostResource and UserResource to prevent duplicuted code

--- All Tasks of Laravel/app/Http/Controllers/Api/PostController.php
+++ All Tasks of Laravel/app/Http/Controllers/Api/PostController.php
@@ -6,27 +6,22 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Http\Requests\StorePostRequest;
-
+use App\Http\Resources\PostResource;
 class PostController extends Controller
 {
     // index function 
     public function index()
     {
         $allPosts = Post::all();
-        return  $allPosts ;
+        // return  $allPosts ;
+        return PostResource::collection($allPosts);
     }
     // show function 
     public function show($postId)
     {
         $post = Post::findOrFail($postId);   
-        // return  $post ;
-        // transformation for data - return associative array of only some attributes
-        return [
-            'id' => $post->id ,
-            'title' => $post->title ,
-            'description' => $post->description ,
-            'user_name' => $post->user->name ,
-        ];
+        // use 
+        return new PostResource($post) ;
     }
     // store function for api will enter as json in postman like {"title":"hello world 44", "description":"description","post_creator": 3 }
       
@@ -39,12 +34,7 @@ class PostController extends Controller
             'user_id' => $data['post_creator'],
             'slug' => str_slug($data['title']),    
         ]);
-        return [
-            'id' => $post->id ,
-            'title' => $post->title ,
-            'description' => $post->description ,
-            'user_name' => $post->user->name ,
-        ];
-        }
+        return new PostResource($post) ;
+    }
 
 }
